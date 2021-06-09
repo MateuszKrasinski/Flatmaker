a8214ea85a71559db5ffcf6d500cea7b8a718de8
Mateusz Krasinski <m.krasinski21@gmail.com>
2021-06-09 15:21:38 +0200

    shoppinglist improvments

--- flatmaker-front/src/js/views/ShoppingList.js
+++ flatmaker-front/src/js/views/ShoppingList.js
@@ -4,6 +4,9 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {faCheckCircle, faMoneyBillAlt, faPlus} from '@fortawesome/free-solid-svg-icons'
 import TopBar from "../components/TopBar";
 import RightBar from "../components/RightBar";
+import React, {useEffect, useState} from "react";
+import {useCookies} from "react-cookie";
+import {postHelp, removeHelp, userHelps} from "../api/Api";
 
 
 function SubPage(pros){
@@ -13,57 +16,96 @@ function SubPage(pros){
         </div>
     )
 }
-function Item(){
-    return (
-        <div className={"item"}>
-            <span>item name</span>
-            <FontAwesomeIcon icon={faMoneyBillAlt}/>
-            <FontAwesomeIcon icon={faCheckCircle}/>
-            <img src={photo} alt=""/>
-        </div>
-    )
-}
-function FridgeContainer(){
-    return (
-
-        <div className={"fridge-container"}>
-            <ItemsContainer/>
-            <ItemAdd/>
-        </div>
-    )
-}
-function ItemsContainer(){
-    return (
-        <div className={"items-container"}>
-            <Item/>
-            <Item/>
-            <Item/>
-
-        </div>
-
-    )
-}
-
-function ItemAdd(){
-    return (
-        <div className={"add-bill"}>
-            <input placeholder={"type bill..."} type="text"/>
-            <FontAwesomeIcon icon={faPlus}/>
-        </div>
-    )
-}
 
 
 
 function OnBoard() {
+    const [projects, setProjects] = useState();
+    const [item, setItem] = useState("");
+    const [state, setState] = useState(".normal-img");
+    const [receiver, setReceiver] = useState();
+    const [groupID] = useState(window.location.href.match(/\d+/g)[1]);
+    const [cookies] = useCookies(['cookie-name']);
+    const changeColor =()=>{
+        console.log(state)
+        if (state === ".normal-img")
+            setState('.selected')
+        else setState('.normal-img')
+    }
+    const removedHelp = async (id) => {
+        console.log(id);
+        const remHelp = await removeHelp({
+            'id':id
+        });
+        await readProjects();
+    }
+    const readProjects = async () => {
+        const getHelp = await userHelps(groupID);
+        setProjects(getHelp);
+        console.log(projects)
+        console.log(item)
+    }
+
+    const addItem = async () => {
+        const getHelp = await postHelp({
+            toID: receiver,
+            fromID: cookies['idUser'],
+            groupID: groupID,
+            name: item,
+            isActive: true,
+            value: 20,
+            type: 1
+        },);
+        console.log(getHelp);
+
+
+    }
+    useEffect(() => readProjects());
     return (
         <div className={'background-white'}>
             <nav>
                 <TopBar/>
-                <SubPage title = {"Shopping List"}/>
+                <SubPage title={"Shopping list"}/>
             </nav>
             <main>
-                <FridgeContainer/>
+
+                <div className={"fridge-container"}>
+                    <div className={"items-container"}>
+                        {
+                            projects !== undefined &&
+                            projects['helps'].map((tech) => {
+                                return (
+                                    <div id={tech['id']} className={"item"}>
+                                        <span>{tech['name']}</span>
+                                        <FontAwesomeIcon icon={faMoneyBillAlt}  id={tech['id']} onClick={(e) => removedHelp(tech['id'])}/>
+                                        <FontAwesomeIcon icon={faCheckCircle} id={tech['id']} onClick={(e) => removedHelp(tech['id'])}/>
+                                        <img src={"/"+tech['id_from']['details']['photo']} alt={""}/>
+                                        <img src={"/"+tech['id_to']['details']['photo']} alt={""}/>
+                                    </div>
+                                )
+
+                            })
+                        }
+
+
+                    </div>
+                    <div className={"add-bill"}>
+                        <input
+                            type="text"
+                            placeholder="type item"
+                            onChange={(e) => setItem(e.target.value)}
+                        ></input>
+                        <FontAwesomeIcon  icon={faPlus} onClick={addItem}/>
+                    </div>
+                </div>
+                <div className={"participants-container"}>
+                    to:
+                    {
+                        projects !== undefined &&
+                        projects['relation'].map((tech) => {
+                            return <img  name={tech['id_user']['id']} className={state} onClick={(e) => setReceiver(e.target.name)} src={"/"+tech['id_user']['details']['photo']} alt=""/>
+                        })}
+                </div>
             </main>
             <RightBar/>
         </div>
